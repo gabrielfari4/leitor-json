337a403e6f7b51f1c5e19e9d74a121003717507d
gabrielfari4 <gabrielfaria88@gmail.com>
2025-03-28 15:26:57 -0300

    :sparkles: feat: funcao separar nomes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { json } from 'stream/consumers';
 
 const caminhoArquivo = process.argv
 const link = caminhoArquivo[2]
@@ -6,12 +7,21 @@ const link = caminhoArquivo[2]
 const lerArquivo = async () => {
     try {
        const contents = await fs.promises.readFile(link, 'utf-8')
-       console.log(contents)
+       parsearESepararNomes(contents)
+    //    console.log(novoContent[0].nome)
     } catch (error) {
         console.log(error)
     }
 }
 
 lerArquivo()
+
+const parsearESepararNomes = (texto) => {
+    const listaTexto = JSON.parse(texto)
+    const nomes = listaTexto.map((passaro, index) => {
+        return `${index +1 }: ${passaro.nome}`
+    })
+    console.log(nomes)
+}
 
 // console.log(caminhoArquivo)
