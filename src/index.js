57743ac95231f0df35d032dd44f658dbfbcba206
gabrielfari4 <gabrielfaria88@gmail.com>
2025-03-31 15:23:18 -0300

    :sparkles: feat: funcao para gerar resultados conforme busca

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import { json } from 'stream/consumers';
 
 const caminhoArquivo = process.argv
 const link = caminhoArquivo[2]
@@ -18,10 +17,35 @@ lerArquivo()
 
 const parsearESepararNomes = (texto) => {
     const listaTexto = JSON.parse(texto)
-    const nomes = listaTexto.map((passaro, index) => {
+    /* const nomes = listaTexto.map((passaro, index) => {
         return `${index +1 }: ${passaro.nome}`
+    }) */
+    // console.log(listaTexto)
+    filtrarPassaro(listaTexto, 'Tityridae')
+}
+
+const filtrarPassaro = (lista, busca) => {
+    const passaros = lista.filter((passaro) => {
+        /* for (const element of passaro) {
+            element.includes(busca)
+        } */
+    //    return passaro.localizacao.includes(busca)
+        // return passaro.filter(prop => prop.includes(busca))
+        return Object.values(passaro).some(valor => {
+            if (typeof valor === 'object') {
+                /* for (const element in valor) {
+                    return element.toLowerCase() === busca
+                } */
+            //    return console.log(valor.includes(busca))
+                const novoArray = valor.map(element => {
+                   return element.toLowerCase()
+                });
+                return novoArray.includes(busca.toLowerCase())
+            }
+            return typeof valor === "string" && valor.toLowerCase().includes(busca.toLowerCase())
+        });
     })
-    console.log(nomes)
+    console.log(passaros) 
 }
 
 // console.log(caminhoArquivo)
